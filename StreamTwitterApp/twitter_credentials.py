250d29315d0a3833d5ce859f98170c71f018be87
Matheusadler <36991359+Matheusadler@users.noreply.github.com>
2020-05-05 20:36:12 -0300

    Files Stream Twitter

---
+++ StreamTwitterApp/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API
+access_token = "1001958840605335552-x64JAExoMfucZ91YWVOEX3FNCbE3On"
+access_token_secret = "KiPZFOhxrdOTDTVjFmLwocKWIzU4Rg6uvewIiMXdzpH3N"
+consumer_key = "gv0ShUD6ebsWSxy2BNjyGJA2J"
+consumer_secret = "DWmxlNNnjsSZG5t0TLf9lh9mceNAecUvkpiBMpd7ztWJ67bNrX"
