4536f151edc5fd4a29faf48d52162b9bdeb3b3ad
Matheus Adler <matheusadler@gmail.com>
2020-08-06 17:43:49 -0300

    Update Project

--- Polyglot App/teste_polyglot.py
+++ Polyglot App/teste_polyglot.py
@@ -6,15 +6,14 @@ with open('../CSV Cleaner/g1_clean.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)
     next(reader)
     for rows in reader:
-        # text = Text(rows)
-        print(rows[0])
-        '''if(text.polarity > 0):
+        text = Text(rows[2])
+        if(text.polarity > 0):
             sentiment = 1
         elif(text.polarity < 0):
             sentiment = -1
         else:
             sentiment = 0
-        print("Nome: ", rows[0], "Sentiment: ", sentiment)'''
+        print("Nome: ", rows[0], "Sentiment: ", sentiment)
 
 
 
